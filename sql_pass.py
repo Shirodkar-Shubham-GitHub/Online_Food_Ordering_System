43c40c1258fd860f2c90199dc191316161ddc2b5
Shubham Shirodkar <shubhamshirodkar90@gmail.com>
2024-08-25 11:47:43 +0530

    First Commit

---
+++ sql_pass.py
@@ -0,0 +1,5 @@
+user_mail = 'shubhamshiro373@gmail.com'
+pass_word = 'ifmv swqf fqpw brph'
+
+host_verify = 'Click the link to verify your account http://Shubham990shirodkar.pythonanywhere.com/verify'
+host_forgot = 'Open the link to change your account password http://http://Shubham990shirodkar.pythonanywhere.com'
