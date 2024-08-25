53817a866a61fb16a8e441724289e1d3b48f6b60
Shubham Ashok Shirodkar <162612412+Shirodkar-Shubham-GitHub@users.noreply.github.com>
2024-08-25 11:48:34 +0530

    Update sql_pass.py

--- sql_pass.py
+++ sql_pass.py
@@ -2,4 +2,4 @@ user_mail = 'shubhamshiro373@gmail.com'
 pass_word = 'ifmv swqf fqpw brph'
 
 host_verify = 'Click the link to verify your account http://Shubham990shirodkar.pythonanywhere.com/verify'
-host_forgot = 'Open the link to change your account password http://http://Shubham990shirodkar.pythonanywhere.com'
+host_forgot = 'Open the link to change your account password http://Shubham990shirodkar.pythonanywhere.com'
