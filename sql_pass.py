eafdd2362b636e00866781cbc189cf1274b5ba4c
Shubham Ashok Shirodkar <162612412+Shirodkar-Shubham-GitHub@users.noreply.github.com>
2024-08-25 15:59:17 +0530

    Delete sql_pass.py

--- sql_pass.py
+++
@@ -1,5 +0,0 @@
-user_mail = 'shubhamshiro373@gmail.com'
-pass_word = 'ifmv swqf fqpw brph'
-
-host_verify = 'Click the link to verify your account http://127.0.0.1:8000/verify'
-host_forgot = 'Open the link to change your account password http://127.0.0.1:8000'
