fe12378ec6c4acaba191b552ab086f3e4194993c
Shubham Ashok Shirodkar <162612412+Shirodkar-Shubham-GitHub@users.noreply.github.com>
2024-10-20 20:13:03 +0530

    Delete config_file.py

--- config_file.py
+++
@@ -1,9 +0,0 @@
-mail_backend = 'django.core.mail.backends.smtp.EmailBackend'
-mail_host = 'smtp.gmail.com'
-user_mail = 'shubhamshiro373@gmail.com'
-pass_word = 'ifmv swqf fqpw brph'
-
-host_verify = 'Click the link to verify your account http://Shubham990shirodkar.pythonanywhere.com/verify'
-host_forgot = 'Open the link to change your account password http://Shubham990shirodkar.pythonanywhere.com'
-
-key_secret = "django-insecure-qv=*@0n3fwu^ll+%c)tmmcfjm%1=^$1#p6kjdpy$-4tmho+d%o"
